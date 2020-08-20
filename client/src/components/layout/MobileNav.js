05b42086b40fb53b3c1a154a3da61a48463df9a7
Brandon Hart <ihartappraisals@gmail.com>
2020-08-20 08:41:19 -0700

    wired up mobile nav functionality

--- client/src/components/layout/MobileNav.js
+++ client/src/components/layout/MobileNav.js
@@ -7,10 +7,22 @@ import HamburgerMenu from 'react-hamburger-menu';
 const MobileNav = () => {
 
 const [isOpen, setIsOpen] = useState(false);
+const [authActionsOpen, setAuthActionsOpen] = useState(false);
 
 const toggleMobileNav = () => {
     setIsOpen(!isOpen);
+    const nav = document.querySelector('.mobile-nav--dropDown-menu');
+    const authActions = document.querySelector('.mobile-nav--dropDown-menu--auth-actions');
+    nav.classList.toggle('visible');
+    if(authActions.classList.contains('visible')) setAuthActionsOpen(false);
+    authActions.classList.remove('visible');
+
 };
+const toggleAuthActions = ()=> {
+    setAuthActionsOpen(!authActionsOpen);
+    const authActions = document.querySelector('.mobile-nav--dropDown-menu--auth-actions');
+    authActions.classList.toggle('visible');
+}
 
 return (
     <Fragment>
@@ -33,7 +45,19 @@ return (
     </div>
     <div>
         <div className='mobile-nav--dropDown-menu'>
+        <HamburgerMenu 
+            isOpen={authActionsOpen}
+            menuClicked={toggleAuthActions}
+            width={18}
+            height={15}
+            strokeWidth={1}
+            rotate={0}
+            color='black'
+            borderRadius={0}
+            animationDuration={0.5}/>
+            <div className='mobile-nav--dropDown-menu--auth-actions'>
 
+            </div>
         </div>
     </div>
     </Fragment>
