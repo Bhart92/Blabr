7afd6e5e29880aed1f113b67be667d14b6c4c977
Brandon Hart <ihartappraisals@gmail.com>
2020-08-20 14:41:41 -0700

    finished mobile styling

--- client/src/components/layout/MobileNav.js
+++ client/src/components/layout/MobileNav.js
@@ -1,10 +1,14 @@
 import React, {Fragment, useState} from 'react';
 import Proptypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import HamburgerMenu from 'react-hamburger-menu';
+import { logout } from '../../actions/auth';
 
-const MobileNav = () => {
+const MobileNav = ({
+    auth: { user },
+    logout
+}) => {
 
 const [isOpen, setIsOpen] = useState(false);
 const [authActionsOpen, setAuthActionsOpen] = useState(false);
@@ -45,7 +49,24 @@ return (
     </div>
     <div>
         <div className='mobile-nav--dropDown-menu'>
-        <HamburgerMenu 
+            <div className='mobile-nav--links'>
+                <ul>
+                    <li onClick={toggleMobileNav}><NavLink exact to='/dashboard' activeClassName='active'><i className="fas fa-home"></i> Home</NavLink></li>
+                    <li onClick={toggleMobileNav}><NavLink exact to='/explore' activeClassName='active' ><i className="fas fa-newspaper"></i> Explore</NavLink></li>
+                    <li onClick={toggleMobileNav}><NavLink exact to='/posts' activeClassName='active' ><i className='fa fa-commenting commenting-NavLink--mobileNav'></i> Posts</NavLink></li>
+                    <li onClick={toggleMobileNav}><NavLink exact to='/profiles' activeClassName='active' ><i className='fa fa-user'></i> People</NavLink></li>
+                </ul>
+            </div>
+        <div className='mobile-nav--greeting-box'>
+        {user.avatar ? (
+                <div className='mobile-nav--greeting-box--image'>
+                    <img src={user.avatar} />
+                    <div className='dashboard--navBar-greeting-box--header'>
+                        <span>Hello {user.firstName}</span>
+                        <span>@{user.handle}</span>
+                    </div>
+                    <HamburgerMenu 
+                    className='inner-burger'
             isOpen={authActionsOpen}
             menuClicked={toggleAuthActions}
             width={18}
@@ -55,8 +76,29 @@ return (
             color='black'
             borderRadius={0}
             animationDuration={0.5}/>
+            </div>
+            ) : (
+                <div className='dashboard--navBar-greeting-box--no-image'>
+                    <i className='fa fa-user'> </i>
+                </div>
+            )}
+        </div>
             <div className='mobile-nav--dropDown-menu--auth-actions'>
+            <div id='modal--container' className='modal--container mobile-modal'> 
+                <div className='dashboard--modal--settings-container'>
+                <div className='modal--settings'>
+                    <span>Delete your account</span>
+                </div>
+                <div className='modal--settings'>
+                <span onClick={toggleMobileNav}><Link to='edit-profile'>Edit your profile</Link></span>
+                </div>
+                <div className='modal--settings'>
+                <span onClick={logout}>Logout</span>
+                </div>
+                </div>
 
+
+            </div>
             </div>
         </div>
     </div>
@@ -65,6 +107,9 @@ return (
     );
 };
 
+const mapStateToProps = state => ({
+    auth: state.auth,
+    profile: state.profile
+  });
 
-
-export default(MobileNav);
+  export default connect(mapStateToProps, { logout })(MobileNav);
